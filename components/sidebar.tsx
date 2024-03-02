122be9106e22863b61ca2e8c3743b8cd8e4afd4a
Bimal Bose B S <bsbimalbose@gmail.com>
2024-03-02 11:59:17 -0500

    active style changes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,13 +1,23 @@
-import Link from "next/link";
-import styles from "./sidebar.module.css";
+import Link from 'next/link';
+import styles from './sidebar.module.css';
+import { useRouter } from 'next/router';
 
 export default function Sidebar() {
+  const router = useRouter();
+
+  const activeStyle = { background: '#323952', color: 'white' };
   return (
     <nav className={styles.nav}>
-      <input className={styles.input} placeholder="Search..." />
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      <input className={styles.input} placeholder='Search...' />
+      <Link style={router.pathname == '/' ? activeStyle : {}} href='/'>
+        Home
+      </Link>
+      <Link style={router.pathname == '/about' ? activeStyle : {}} href='/about'>
+        About
+      </Link>
+      <Link style={router.pathname == '/contact' ? activeStyle : {}} href='/contact'>
+        Contact
+      </Link>
     </nav>
   );
 }
